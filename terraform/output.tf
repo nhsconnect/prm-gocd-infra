766b40be1794825c7325b5e78368993515d1ea42
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-11-29 10:24:33 +0000

    [PRMT-756] GoCD server provisioned by ansible

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,3 +6,11 @@ output "agent_ips" {
 output "server_public_ip" {
   value = aws_instance.gocd_server.public_ip
 }
+
+output "environment" {
+  value = var.environment
+}
+
+output "remote_user" {
+  value = local.remote_user
+}
