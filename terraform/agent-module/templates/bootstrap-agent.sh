66eb04670a8256b50b112d005dfe0430e30fba66
ewrobel-nhs <edyta.wrobel@nhs.net>
2021-01-15 16:34:23 +0000

    [PRMT-1128] Updated GoCD server url to be accessible on port 443. Fix auto register for the agent resources to be read from env variable.

--- terraform/agent-module/templates/bootstrap-agent.sh
+++ terraform/agent-module/templates/bootstrap-agent.sh
@@ -6,7 +6,7 @@ AWS_REGION=${AWS_REGION}
 AGENT_RESOURCES=${AGENT_RESOURCES}
 
 if hash docker 2>/dev/null; then
-  echo "Docker aleady installed"
+  echo "Docker already installed"
 else
   sudo yum update -y
   sudo amazon-linux-extras install -y docker
@@ -30,18 +30,14 @@ docker run -d \
   --cap-add SYS_ADMIN \
   --security-opt apparmor:unconfined \
   -e AGENT_HOSTNAME=$HOSTNAME \
-  -e AGENT_RESOURCES=$AGENT_RESOURCES \
+  -e AGENT_AUTO_REGISTER_RESOURCES=$AGENT_RESOURCES \
   -e GOCD_ENVIRONMENT=$GOCD_ENVIRONMENT \
   -e AWS_REGION=$AWS_REGION \
   -e DOCKER_OPTS="--storage-driver overlay2" \
-  -e GO_SERVER_URL="https://$GOCD_ENVIRONMENT.gocd.patient-deductions.nhs.uk:8153/go" \
+  -e GO_SERVER_URL="https://$GOCD_ENVIRONMENT.gocd.patient-deductions.nhs.uk/go" \
   -e AGENT_BOOTSTRAPPER_ARGS="-sslVerificationMode NONE" \
-  -e AWS_SECRET_STORE_PATH="repo/$GOCD_ENVIRONMENT/user-input" \
-  -e GOCD_SKIP_SECRETS="true" \
-  -e SECRET_STORE="aws" \
   -v "/var/go-agent/godata:/godata" \
   -v "/var/go-agent/docker:/var/lib/docker" \
-  -v "/var/go-agent/workspace:/var/lib/go-agent/pipelines" \
   -v "/etc/localtime:/etc/localtime:ro" \
   -v "/lib/modules:/lib/modules:ro" \
   -v "/sys/fs/cgroup:/sys/fs/cgroup" \
