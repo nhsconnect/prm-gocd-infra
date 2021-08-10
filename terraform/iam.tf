4f25d07a2383cd4be84a8c720f013a53a16c23b7
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-08-10 13:49:24 +0100

    [PRMT-1905] Deleted unused gocd-server role, instance profile and policy attachments

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,20 +9,6 @@ data "aws_iam_policy_document" "instance-assume-role-policy" {
   }
 }
 
-resource "aws_iam_role" "gocd_server" {
-  name                 = "gocd_server-${var.environment}"
-  assume_role_policy   = data.aws_iam_policy_document.instance-assume-role-policy.json
-  tags = {
-    CreatedBy   = var.repo_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_iam_instance_profile" "gocd_server" {
-  name = "gocd_server-${var.environment}"
-  role = aws_iam_role.gocd_server.name
-}
-
 resource "aws_iam_role" "gocd_agent" {
   name                 = "gocd_agent-${var.environment}"
   assume_role_policy   = data.aws_iam_policy_document.instance-assume-role-policy.json
@@ -35,19 +21,6 @@ resource "aws_iam_role" "gocd_agent" {
 resource "aws_iam_instance_profile" "gocd_agent" {
   name = "gocd_agent-${var.environment}"
   role = aws_iam_role.gocd_agent.name
-}
-
-# Server instance needs to pull the GoCD docker image
-resource "aws_iam_role_policy_attachment" "server-ecr-attach" {
-  role       = aws_iam_role.gocd_server.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-#For DNS challenge and ssl certs
-#FIXME: restrict access to public zone only
-resource "aws_iam_role_policy_attachment" "server-r53-attach" {
-  role       = aws_iam_role.gocd_server.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
 }
 
 #TODO: other permissions for GoCD server, agent
