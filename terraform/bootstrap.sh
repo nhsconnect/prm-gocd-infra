f88b564202b2d4b06af075d2930f788801d99025
Oktawia Kata <oktawia.kata1@nhs.net>
2021-10-11 11:48:08 +0100

    [PRMT-2394] Remove insecure CBC ciphers from sshd config in GoCD server

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
+# Remove insecure CBC ciphers from sshd config
+sudo sshd -T | grep ciphers | sudo sed -e "s/\(3des-cbc\|aes128-cbc\|aes192-cbc\|aes256-cbc\|blowfish-cbc\|cast128-cbc\)\,\?//g" | sudo tee -a /etc/ssh/sshd_config
+
 if hash docker 2>/dev/null; then
-  echo "Docker aleady installed"
+  echo "Docker already installed"
 else
   sudo yum update -y
   sudo amazon-linux-extras install -y docker
