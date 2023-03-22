5f111fea4167cda2a8d4902a73f0c02f1106f463
Caius Eugene <caius.eugene1@nhs.net>
2023-03-22 10:38:43 +0000

    Updated infra diagram to correct subnets

--- docs/infra-diagram.py
+++ docs/infra-diagram.py
@@ -1,22 +1,26 @@
 from diagrams import Cluster, Diagram
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
+from diagrams.aws.general import InternetAlt2
 from diagrams.aws.management import ParameterStore
 from diagrams.aws.network import ELB, ClientVpn, VPC, NATGateway, InternetGateway, Route53
 
 with Diagram("GoCD Infrastructure Diagram", show=False):
     vpn = ClientVpn("AWS VPN")
+    internet = InternetAlt2("Public Internet")
 
     with Cluster("VPC"):
 
         ssm = ParameterStore("SSM")
         r53 = Route53("DNS")
 
+        with Cluster("DB Subnet"):
+            gocd_db_volume = RDS("GoCD DB Cluster")
+            gocd_db_instance = EC2("GoCD DB Instance")
+
         with Cluster("Private Subnet"):
             igw = InternetGateway("Internet Gateway")
             gocd_server = EC2("GoCD Server")
-            gocd_db_volume = RDS("GoCD DB Cluster")
-            gocd_db_instance = EC2("GoCD DB Instance")
 
             with Cluster("Agents"):
                 gocd_agents = [EC2("GoCD Agent 0"), EC2("GoCD Agent 1"), EC2("GoCD Agent 2")]
@@ -30,3 +34,4 @@ with Diagram("GoCD Infrastructure Diagram", show=False):
     gocd_server >> gocd_db_volume >> gocd_db_instance
     gocd_agents >> ngw
     gocd_server >> r53
+    ngw >> internet
